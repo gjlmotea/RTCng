c432dd5d8df798c067abf51dea1f09eddd8050e0
GJLMoTea <gjlmotea@gmail.com>
2022-03-04 17:10:01 +0800

    is camera on

--- src/app/web-rtc/web-rtc.component.ts
+++ src/app/web-rtc/web-rtc.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+import {Subscription, timer} from 'rxjs';
 
 @Component({
   selector: 'app-web-rtc',
@@ -6,41 +7,58 @@ import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleC
   styleUrls: ['./web-rtc.component.css']
 })
 export class WebRtcComponent implements OnInit, OnChanges, AfterViewInit {
-  @ViewChild('camera', { static: true }) camera: ElementRef;
+  @ViewChild('camera', {static: true}) camera: ElementRef;
   @Input() public isRecording = false;
+  public isCameraOn = false;
+  public defaultSec = 60;
+  public displaySec = 0;
+  private countDown$: Subscription;
+
 
   constructor() {
-    let mediaRecorder;
-    let chunks = [];
-    const record$ = document.querySelector('#record');
-    const playback$ = document.querySelector('#playback');
-    const timer$ = document.querySelector('#timer');
-    const memoryUsage$ = document.querySelector('#memoryUsage');
 
   }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
   }
 
-  ngAfterViewInit(): void {
+  public ngAfterViewInit(): void {
     const constraints = {
       audio: true,
       video: {
-        width: { exact: 1280 },
-        height: { exact: 720 },
+        width: {exact: 1280},
+        height: {exact: 720},
         //   width: { min: 1024, ideal: 1280, max: 1920 },
         //   height: { min: 576, ideal: 720, max: 1080 },
       },
     };
+
+    // 開啟鏡頭
     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
       this.camera.nativeElement.srcObject = stream;
+      this.isCameraOn = true;
     }).catch((err) => {
       console.error(err);
     });
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    console.log(changes);
+  public ngOnChanges(changes: SimpleChanges) {
+    for (const propName of Object.keys(changes)) {
+      const tick = timer(1000, 1000);
+
+      // 當錄影開始
+      if (changes.isRecording.currentValue === true && changes.isRecording.previousValue === false) {
+        this.displaySec = this.defaultSec;
+        this.countDown$ = tick.subscribe(sec => {
+          this.displaySec = this.defaultSec - (sec + 1);
+        } );
+      }
+
+      // 當結束錄影
+      if (changes.isRecording.currentValue === false && changes.isRecording.previousValue === true) {
+        this.countDown$.unsubscribe();
+      }
+    }
   }
 
 }
